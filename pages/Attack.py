fd7bc5bfc1717a52c5964f27b7e4868614835d94
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 15:00:05 +0200

    Update Attack.py

--- pages/Attack.py
+++ pages/Attack.py
@@ -1,6 +1,22 @@
 import streamlit as st
 from PIL import Image
 from pages import Anxiety_Attack_Protocol, Anxiety_protocol
+import os
+
+def switch_pages(page_name):
+    """Switches to the specified page."""
+    if page_name == "Anxiety_protocol":
+        file_path = os.path.join("pages", "Anxiety_protocol.py")
+        if os.path.exists(file_path):
+            st.success("Redirecting to Anxiety Protocol page...")
+            os.system("streamlit run pages/Anxiety_protocol.py")
+        else:
+            st.error("Anxiety_protocol.py file not found.")
+    elif page_name == "Anxiety_Attack_Protocol":
+        st.success("Redirecting to Anxiety Attack Protocol page...")
+        # Fügen Sie hier die Logik hinzu, um zur Seite Anxiety_Attack_Protocol zu wechseln
+    else:
+        st.error("Unknown page: {}".format(page_name))
 
 def show():
     st.image("Logo.jpeg", width=600)
@@ -8,27 +24,15 @@ def show():
     
     st.write("Anxiety Assessment:")
     
-    st.write("Do you feel like you're having an Anxiety Attack right now?")
-    if st.button("Yes"):
-        anxiety_attack_protocol()
-    elif st.button("No"):
-        redirect_question_2()
-        if st.button("Yes "):
-            anxiety_protocol()
-        elif st.button("No "):
-            No_2_question()
-
-def anxiety_attack_protocol():
-    st.write("Redirecting to Anxiety Attack Protocol page...")
-
-def redirect_question_2():
-    st.write("Are you anxious right now?")
-
-def anxiety_protocol():
-    st.write("Redirecting to Anxiety Protocol page...")
-
-def No_2_question():
-    st.experimental_rerun()
+    answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
+    if answer == "Yes":
+        switch_pages("Anxiety_Attack_Protocol")
+    else:
+        answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
+        if answer_2 == "Yes":
+            switch_pages("Anxiety_protocol")
+        else:
+            st.write("Reassess your feelings.")
 
 if __name__ == "__main__":
     show()
