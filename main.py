a0eb86d4eeac8d267bcbb159efb959946ddd3b8f
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 15:13:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,29 @@ PAGE_TITLE_MAP = {
     "Anxiety Protocol Page": Anxiety_protocol
 }
 
+import streamlit as st
+from PIL import Image
+import navigation  # Importieren Sie die navigation.py-Datei
+
+def show():
+    st.image("Logo.jpeg", width=600)
+    st.write("---")
+    
+    st.write("Anxiety Assessment:")
+    
+    answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
+    if answer == "Yes":
+        navigation.switch_pages("Anxiety_Attack_Protocol")  # Verwenden Sie die switch_pages()-Funktion
+    else:
+        answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
+        if answer_2 == "Yes":
+            navigation.switch_pages("Anxiety_protocol")  # Verwenden Sie die switch_pages()-Funktion
+        else:
+            st.write("Reassess your feelings.")
+
+if __name__ == "__main__":
+    show()
+
 if __name__ == "__main__":
     Mainpage()
 
