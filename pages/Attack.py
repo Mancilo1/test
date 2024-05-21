3b6151bb151925127416f1366837fbb632563798
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 15:35:41 +0200

    Update Attack.py

--- pages/Attack.py
+++ pages/Attack.py
@@ -2,16 +2,6 @@ import streamlit as st
 from PIL import Image
 import time
 
-def switch_pages(page_name):
-    if page_name == "Anxiety_Attack_Protocol":
-        st.success("Redirecting to Anxiety Attack Protocol page...")
-        time.sleep(2)
-        st.switch_page("Anxiety_Attack_Protocol.py")
-    elif page_name == "Anxiety_protocol":
-        st.success("Redirecting to Anxiety Protocol page...")
-        time.sleep(2)
-        st.switch_page("Anxiety_Protocol.py")
-
 def show():
     st.image("Logo.jpeg", width=600)
     st.write("---")
@@ -20,11 +10,11 @@ def show():
     
     answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
     if answer == "Yes":
-        switch_pages("Anxiety_Attack_Protocol")
+        switch_pages("Anxiety_Attack_Protocol.py")
     else:
         answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
         if answer_2 == "Yes":
-            switch_pages("Anxiety_protocol")
+            switch_pages("Anxiety_protocol.py")
         else:
             st.write("Reassess your feelings.")
 
