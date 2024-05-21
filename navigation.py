569668be590e50436bd2194249a30f42d025f34d
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 15:11:26 +0200

    Create navigation.py

---
+++ navigation.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+
+def switch_pages(page_name):
+    if page_name == "Anxiety_Attack_Protocol":
+        st.success("Redirecting to Anxiety Attack Protocol page...")
+        # Fügen Sie hier die Logik hinzu, um zur Seite Anxiety_Attack_Protocol zu wechseln
+    elif page_name == "Anxiety_protocol":
+        st.success("Redirecting to Anxiety Protocol page...")
+        # Fügen Sie hier die Logik hinzu, um zur Seite Anxiety_protocol zu wechseln
