60538ee3b70634bddb710142869f3bbfc0bb40ef
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 17:03:41 +0200

    Update Anxiety_Attack_Protocol.py

--- pages/Anxiety_Attack_Protocol.py
+++ pages/Anxiety_Attack_Protocol.py
@@ -15,9 +15,6 @@ def read_csv(filename='anxiety_data.csv'):
             return list(reader)
     return []
 
-def show():
-    st.title("Anxiety Attack Protocol")
-
 def anxiety_attack_protocol():
     # Check if the session state object exists, if not, initialize it
     if 'button_count' not in st.session_state:
@@ -38,69 +35,47 @@ def anxiety_attack_protocol():
     # Question 3: Symptoms
     st.subheader("Symptoms:")
     col1, col2 = st.columns(2)
-    symptoms = []
     with col1:
-        if st.checkbox("Anxiety"):
-            symptoms.append("Anxiety")
-        if st.checkbox("Chest Pain"):
-            symptoms.append("Chest Pain")
-        if st.checkbox("Chills"):
-            symptoms.append("Chills")
-        if st.checkbox("Chocking"):
-            symptoms.append("Chocking")
-        if st.checkbox("Cold"):
-            symptoms.append("Cold")
-        if st.checkbox("Cold Hands"):
-            symptoms.append("Cold Hands")
-        if st.checkbox("Dizziness"):
-            symptoms.append("Dizziness")
-        if st.checkbox("Feeling of danger"):
-            symptoms.append("Feeling of danger")
-        if st.checkbox("Feeling of dread"):
-            symptoms.append("Feeling of dread")
-        if st.checkbox("Heart racing"):
-            symptoms.append("Heart racing")
-        if st.checkbox("Hot flushes"):
-            symptoms.append("Hot flushes")
-        if st.checkbox("Irrational thinking"):
-            symptoms.append("Irrational thinking")
+        symptoms_anxiety = st.checkbox("Anxiety")
+        symptoms_chestpain = st.checkbox("Chest Pain")
+        symptoms_chills = st.checkbox("Chills")
+        symptoms_chocking = st.checkbox("Chocking")
+        symptoms_cold = st.checkbox("Cold")
+        symptoms_coldhands = st.checkbox("Cold Hands")
+        symptoms_dizziness = st.checkbox("Dizziness")
+        symptoms_feelingdanger = st.checkbox("Feeling of danger")
+        symptoms_feelingdread = st.checkbox("Feeling of dread")
+        symptoms_heartracing = st.checkbox("Heart racing")
+        symptoms_hotflushes = st.checkbox("Hot flushes")
+        symptoms_irrationalthinking = st.checkbox("Irrational thinking")
     with col2:
-        if st.checkbox("Nausea"):
-            symptoms.append("Nausea")
-        if st.checkbox("Nervousness"):
-            symptoms.append("Nervousness")
-        if st.checkbox("Numb Hands"):
-            symptoms.append("Numb Hands")
-        if st.checkbox("Numbness"):
-            symptoms.append("Numbness")
-        if st.checkbox("Palpitations"):
-            symptoms.append("Palpitations")
-        if st.checkbox("Shortness of Breath"):
-            symptoms.append("Shortness of Breath")
-        if st.checkbox("Sweating"):
-            symptoms.append("Sweating")
-        if st.checkbox("Tense Muscles"):
-            symptoms.append("Tense Muscles")
-        if st.checkbox("Tingly Hands"):
-            symptoms.append("Tingly Hands")
-        if st.checkbox("Trembling"):
-            symptoms.append("Trembling")
-        if st.checkbox("Tremor"):
-            symptoms.append("Tremor")
-        if st.checkbox("Weakness"):
-            symptoms.append("Weakness")
+        symptoms_nausea = st.checkbox("Nausea")
+        symptoms_nervous = st.checkbox("Nervousness")
+        symptoms_numbhands = st.checkbox("Numb Hands")
+        symptoms_numbness = st.checkbox("Numbness")
+        symptoms_palpitations = st.checkbox("Palpitations")
+        symptoms_shortbreath = st.checkbox("Shortness of Breath")
+        symptoms_sweating = st.checkbox("Sweating")
+        symptoms_tensemuscles = st.checkbox("Tense Muscles")
+        symptoms_tinglyhands = st.checkbox("Tingly Hands")
+        symptoms_trembling = st.checkbox("Trembling")
+        symptoms_tremor = st.checkbox("Tremor")
+        symptoms_weakness = st.checkbox("Weakness")
     
+    if 'symptoms' not in st.session_state:
+        st.session_state.symptoms = []
+
     new_symptom = st.text_input("Add new symptom:")
     if st.button("Add Symptom") and new_symptom:
         st.session_state.symptoms.append(new_symptom)
-        symptoms.append(new_symptom)
+
+    for symptom in st.session_state.symptoms:
+        st.write(symptom)
 
     # Question 4: Triggers
     st.subheader("Triggers:")
-    selected_triggers = st.multiselect("Select Triggers", ["Stress", "Caffeine", "Lack of Sleep", "Social Event", "Reminder of traumatic event", "Alcohol", "Conflict", "Family problems"])
-    if selected_triggers:
-        st.session_state.triggers.extend(selected_triggers)
-
+    triggers = st.multiselect("Select Triggers", ["Stress", "Caffeine", "Lack of Sleep", "Social Event", "Reminder of traumatic event", "Alcohol", "Conflict", "Family problems"])
+    
     new_trigger = st.text_input("Add new trigger:")
     if st.button("Add Trigger") and new_trigger:
         st.session_state.triggers.append(new_trigger)
@@ -118,8 +93,8 @@ def anxiety_attack_protocol():
         data = [
             date_selected,
             "; ".join([f"{time.strftime('%H:%M')} - {severity}" for time, severity in time_severity_pairs]),
-            ", ".join(symptoms),
-            ", ".join(st.session_state.triggers),
+            ", ".join([symptom for symptom in st.session_state.symptoms]),
+            ", ".join(triggers + st.session_state.triggers),
             help_response
         ]
         save_to_csv(data)
@@ -128,7 +103,6 @@ def anxiety_attack_protocol():
 def add_time_severity():
     st.subheader("Time & Severity")
     
-    # Initialize times list if not already initialized
     if 'times' not in st.session_state:
         st.session_state.times = []
 
@@ -139,18 +113,12 @@ def add_time_severity():
             time_selected = datetime.datetime.now().time()
             severity = 1
 
-        # Convert time_selected to string with minute precision
         time_selected_str = time_selected.strftime('%H:%M')
-        
-        # Display time input with minute precision
-        time_selected_str = st.text_input(f"Time {i+1}", value=time_selected_str)
-        
-        # Convert the string back to datetime.time object
+        time_selected_str = st.text_input(f"Time {i+1}", value=time_selected_str, key=f"time_input_{i}")
         time_selected = datetime.datetime.strptime(time_selected_str, '%H:%M').time()
         
-        severity = st.slider(f"Severity (1-10) {i+1}", min_value=1, max_value=10, value=severity)
+        severity = st.slider(f"Severity (1-10) {i+1}", min_value=1, max_value=10, value=severity, key=f"severity_slider_{i}")
         
-        # Update time and severity in session state
         if len(st.session_state.times) <= i:
             st.session_state.times.append((time_selected, severity))
         else:
@@ -163,7 +131,6 @@ def main_page():
     st.title("FeelNow")
     anxiety_attack_protocol()
 
-    # Display saved data
     st.header("Saved Data")
     data = read_csv()
     if data:
