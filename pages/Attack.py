177b008d76969cc73e701d018c67761271d44f03
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-21 15:15:05 +0200

    Update Attack.py

--- pages/Attack.py
+++ pages/Attack.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 from pages import Anxiety_Attack_Protocol, Anxiety_protocol
+import os
 
 def show():
     st.image("Logo.jpeg", width=600)
@@ -17,6 +18,19 @@ def show():
             anxiety_protocol()
         elif st.button("No "):
             No_2_question()
+
+def switch_page(page_name):
+    if page_name == "Anxiety_protocol":
+        file_path = os.path.join("pages", "Anxiety_protocol.py")
+        if os.path.exists(file_path):
+            os.system("streamlit run pages/Anxiety_protocol.py")
+        else:
+            st.error("Anxiety_protocol.py file not found.")
+    elif page_name == "anxiety_attack_protocol":
+        # Logik für die Dashboard-Seite
+        st.title("pages", "Anxiety_Attack_protocol")
+        st.write("Switched to Dashboard page.")
+
 
 def anxiety_attack_protocol():
     st.write("Redirecting to Anxiety Attack Protocol page...")
