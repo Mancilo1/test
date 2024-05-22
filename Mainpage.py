502351ef7268d5670e6c9102ec5eed4b671eb797
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 09:44:47 +0200

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -1,6 +1,25 @@
 import streamlit as st
 from PIL import Image  
+import streamlit as st
+from pages import Mainpage
+from pages import Login
+from pages import Attack 
+from pages import Anxiety_Attack_Protocol
+from pages import Anxiety_protocol
+from github_contents import GithubContents
 
+github = GithubContents(
+    st.secrets["github"]["owner"],
+    st.secrets["github"]["repo"],
+    st.secrets["github"]["token"])
+
+PAGE_TITLE_MAP = {
+    "Main Page": Mainpage,
+    "Login Page": Login,
+    "Register Page": Attack,
+    "Anxiety Attack Protocol Page": Anxiety_Attack_Protocol,
+    "Anxiety Protocol Page": Anxiety_protocol
+}
 def show():
     st.title("Main Page")
 
