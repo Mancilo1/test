46fec272a17f704928238c22f05fd09e645c037e
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 10:15:40 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -2,15 +2,17 @@ import streamlit as st
 from PIL import Image
 import time
 
+import pages.anxiety_attack_protocol as attack_protocol
+import pages.anxiety_protocol as anxiety_protocol
+
 def main():
+    # Get query parameters to determine the page
     query_params = st.experimental_get_query_params()
     page = query_params.get("page", ["main"])[0]
 
     if page == "anxiety_attack_protocol":
-        import pages.anxiety_attack_protocol as attack_protocol
         attack_protocol.show()
     elif page == "anxiety_protocol":
-        import pages.anxiety_protocol as anxiety_protocol
         anxiety_protocol.show()
     else:
         show_main_page()
@@ -18,9 +20,9 @@ def main():
 def show_main_page():
     st.image("Logo.jpeg", width=600)
     st.write("---")
-    
+
     st.write("Anxiety Assessment:")
-    
+
     answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
     if answer == "Yes":
         switch_pages("anxiety_attack_protocol")
