00d32bb41301711bbfb7d066395087762c6c8df4
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 11:32:35 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -28,11 +28,11 @@ def show():
 
     answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
     if answer == "Yes":
-        switch_pages("anxiety_attack_protocol")
+        st.switch_pages("pages/anxiety_attack_protocol.py")
     else:
         answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
         if answer_2 == "Yes":
-            switch_pages("anxiety_protocol")
+            st.switch_pages("pages/anxiety_protocol.py")
         else:
             st.write("Reassess your feelings.")
 
