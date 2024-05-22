e54f0ae6563d568b00f93f217e4574b7efdcb295
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-22 10:05:25 +0200

    Update attack.py

--- pages/attack.py
+++ pages/attack.py
@@ -10,17 +10,17 @@ def show():
     
     answer = st.radio("Do you feel like you're having an Anxiety Attack right now?", ("Yes", "No"))
     if answer == "Yes":
-        st.switch_pages("pages/anxiety_attack_protocol.py")  # Hier wird der Dateiname als Zeichenkette übergeben
+        st.switch_pages("pages/anxiety_attack_protocol.py")
     else:
         answer_2 = st.radio("Are you anxious right now?", ("Yes", "No"))
         if answer_2 == "Yes":
-            st.switch_pages("pages/anxiety_protocol.py")  # Hier wird der Dateiname als Zeichenkette übergeben
+            st.switch_pages("pages/anxiety_protocol.py")
         else:
             st.write("Reassess your feelings.")
 
 def switch_pages(page_name):
-    st.success("Redirecting to {} page...".format(page_name))  # Hier können Sie eine Erfolgsmeldung oder eine andere Benachrichtigung anzeigen
-    time.sleep(2)  # Optional: Kleine Verzögerung, um die Nachricht anzuzeigen
+    st.success("Redirecting to {} page...".format(page_name))
+    time.sleep(2)
     st.experimental_rerun()
 
 if __name__ == "__main__":
