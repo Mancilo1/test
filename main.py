b3e4d278459e4cc52cd07f5b8d1be9e0e28eb872
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 22:41:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def main_sidebar():
         st.sidebar.switch_page("main.py")
 
 def main_page():
+    main_sidebar()
+    
     st.image("Logo.jpeg", width=600)
     st.subheader("Anxiety Tracker Journal")
     st.write("""
