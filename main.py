9f662f33aed1771b1afd22a89f02b88413f19f3e
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 17:59:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ github = GithubContents(
 def main_page():
     # Sidebar configuration
     st.sidebar.image("Logo.jpeg", use_column_width=True)
-    st.sidebar.title("Navigation")
-    page_selection = st.sidebar.selectbox("Select a page", ["Main", "Login/Register"])
 
     # Main page content
     if page_selection == "Main":
