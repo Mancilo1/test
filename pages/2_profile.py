a412d09bf91bdad0bc19ad5689305565aec20bae
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 21:31:22 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -42,6 +42,10 @@ def main_page():
             st.write("Name:", user_data['name'].iloc[0])
             st.write("Birthday:", user_data['birthday'].iloc[0])
             st.sidebar.write(f"Logged in as {st.session_state['username']}")
+            logout_button = st.sidebar.button("Logout")
+            if logout_button:
+            st.session_state['authentication'] = False
+            st.session_state.pop('username', None)
         else:
             st.error("User data not found.")
     else:
