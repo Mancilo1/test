48c63d51d5108e0089c5b1348103ae7c1e83827f
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 23:16:46 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -42,7 +42,7 @@ def anxiety_assessment():
         st.write("Are you anxious right now?")
         if st.button("Yes "):
             switch_page("pages/5_anxiety_protocol.py")
-        elif st.button("No"):
+        elif st.button("No "):
             gif_url = "https://37.media.tumblr.com/28fad0005f6861c08f2c07697ff74aa4/tumblr_n4y0patw7Q1rn953bo1_500.gif"
             st.image(gif_url, width=400)
 
