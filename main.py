02ede60fade74e6f020e855d0b458d5b84bfa150
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 22:29:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def main_sidebar():
     st.sidebar.title("Navigation")
     page = st.sidebar.selectbox("Choose a page", ["Profile", "Anxiety Attack", "Anxiety", "Mainpage"])
     if page == "Profile":
-        st.switch_page("pages/2_profile.py")
+        st.sidebar.switch_page("pages/2_profile.py")
     elif page == "Anxiety Attack":
-        st.switch_page("pages/4_anxiety_attack_protocol.py")
+        st.sidebar.switch_page("pages/4_anxiety_attack_protocol.py")
     elif page == "Anxiety":
-        st.switch_page("pages/5_anxiety_protocol.py")
+        st.sidebar.switch_page("pages/5_anxiety_protocol.py")
     elif page == "Mainpage":
-        st.switch_page("main.py")
+        st.sidebar.switch_page("main.py")
 
 def main_page():
     st.image("Logo.jpeg", width=600)
