0bf578e147113ca84e3414b83096256a2c2e0a50
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 23:49:51 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -36,18 +36,15 @@ def anxiety_assessment():
     st.subheader("Anxiety Assessment:")
     st.write("Do you feel like you're having an Anxiety Attack right now?")
     if st.button("Yes"):
-        st.session_state.assessment_selection = "attack"
-        st.experimental_rerun()
+        st.switch_page("pages/4_anxiety_attack_protocol.py")
     if st.button("No"):
-        st.session_state.assessment_selection = "anxiety"
-        st.experimental_rerun()
+        anxiety_assessment2()
 
 def anxiety_assessment2():
     st.write("Are you anxious right now?")
-    if st.button("Yes"):
-        st.session_state.assessment_selection = "protocol"
-        st.experimental_rerun()
-    elif st.button("No"):
+    if st.button("Yes "):
+        st.switch_page("pages/5_anxiety_protocol.py")
+    elif st.button("No "):
         st.markdown(gif_html, unsafe_allow_html=True)
         gif_url = "https://64.media.tumblr.com/28fad0005f6861c08f2c07697ff74aa4/tumblr_n4y0patw7Q1rn953bo1_500.gif"
         gif_html = f'<img src="{gif_url}" width="400" height="300">'
