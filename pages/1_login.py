cd7befddd761c3e108ea0cb8cce1e795aa4caac8
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 18:41:25 +0200

    Update 1_login.py

--- pages/1_login.py
+++ pages/1_login.py
@@ -34,7 +34,7 @@ def register_page():
         new_first_name = st.text_input("First Name")
         new_last_name = st.text_input("Last Name")
         new_username = st.text_input("Username")
-        new_birthday = st.date_input("Birthday", min_value=datetime.date(1900, 1, 1))
+        new_birthday = st.date_input("Birthday")
         new_password = st.text_input("Password", type="password")
         
         # Hier fügst du den Submit-Button hinzu
