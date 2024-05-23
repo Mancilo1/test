fb5f46f94bd69aeba6740212ee0d60cebf7b8e1f
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 22:17:46 +0200

    Update 5_anxiety_protocol.py

--- pages/5_anxiety_protocol.py
+++ pages/5_anxiety_protocol.py
@@ -1,91 +1,39 @@
-import binascii
 import streamlit as st
-import pandas as pd
 import bcrypt
-from github_contents import GithubContents
+import binascii
+import pytz
 import datetime
+import pandas as pd
+from github_contents import GithubContents
 
 # Constants
 DATA_FILE = "MyLoginTable.csv"
 DATA_COLUMNS = ['username', 'name', 'password']
 
-def show():
-    st.title("Login/Register")
+def main():
+    init_github()
+    init_credentials()
 
-def login_page():
-    """ Login an existing user. """
-    st.title("Login")
-    with st.form(key='login_form'):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        if st.form_submit_button("Login"):
-            authenticate(username, password)
+    if 'authentication' not in st.session_state:
+        st.session_state['authentication'] = False
 
-def register_page():
-    """ Register a new user. """
-    st.title("Register")
-    with st.form(key='register_form'):
-        new_username = st.text_input("New Username")
-        new_name = st.text_input("Name")
-        new_password = st.text_input("New Password", type="password")
-        if st.form_submit_button("Register"):
-            hashed_password = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())
-            hashed_password_hex = binascii.hexlify(hashed_password).decode()
+    if not st.session_state['authentication']:
+        options = st.sidebar.selectbox("Select a page", ["Login", "Register"])
+        if options == "Login":
+            login_page()
+        elif options == "Register":
+            register_page()
             
-            # Check if the username already exists
-            if new_username in st.session_state.df_users['username'].values:
-                st.error("Username already exists. Please choose a different one.")
-                return
-            else:
-                new_user = pd.DataFrame([[new_username, new_name, hashed_password_hex]], columns=DATA_COLUMNS)
-                st.session_state.df_users = pd.concat([st.session_state.df_users, new_user], ignore_index=True)
-                
-                # Writes the updated dataframe to GitHub data repository
-                st.session_state.github.write_df(DATA_FILE, st.session_state.df_users, "added new user")
-                st.success("Registration successful! You can now log in.")
-
-def authenticate(username, password):
-    """
-    Authenticate the user.
-
-    Parameters:
-    username (str): The username to authenticate.
-    password (str): The password to authenticate.
-    """
-    login_df = st.session_state.df_users
-    login_df['username'] = login_df['username'].astype(str)
-
-    if username in login_df['username'].values:
-        stored_hashed_password = login_df.loc[login_df['username'] == username, 'password'].values[0]
-        stored_hashed_password_bytes = binascii.unhexlify(stored_hashed_password)
-        
-        # Check the input password
-        if bcrypt.checkpw(password.encode('utf8'), stored_hashed_password_bytes): 
-            st.session_state['authentication'] = True
-            st.session_state['username'] = username
-            st.success('Login successful')
-            st.experimental_rerun()
-        else:
-            st.error('Incorrect password')
     else:
-        st.error('Username not found')
+        st.sidebar.write(f"Logged in as {st.session_state['username']}")
+        anxiety_protocol()
 
-def init_github():
-    """Initialize the GithubContents object."""
-    if 'github' not in st.session_state:
-        st.session_state.github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"])
-        print("github initialized")
-    
-def init_credentials():
-    """Initialize or load the dataframe."""
-    if 'df_users' not in st.session_state:
-        if st.session_state.github.file_exists(DATA_FILE):
-            st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
-        else:
-            st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
+        logout_button = st.sidebar.button("Logout")
+        if logout_button:
+            st.session_state['authentication'] = False
+            st.session_state.pop('username', None)
+            st.switch_page("main.py")
+            st.experimental_rerun()
 
 def anxiety_protocol():
     username = st.session_state['username']
@@ -178,30 +126,111 @@ def anxiety_protocol():
     st.subheader("Saved Entries")
     st.write(st.session_state.anxiety_data)
 
-def main():
-    init_github()
-    init_credentials()
+def add_time_severity():
+    if 'time_severity_entries' not in st.session_state:
+        st.session_state.time_severity_entries = []
 
-    if 'authentication' not in st.session_state:
-        st.session_state['authentication'] = False
+    st.subheader("Time & Severity")
 
-    if not st.session_state['authentication']:
-        options = st.sidebar.selectbox("Select a page", ["Login", "Register"])
-        if options == "Login":
-            login_page()
-        elif options == "Register":
-            register_page()
+    # Display the current time
+    current_time = datetime.datetime.now(pytz.timezone('Europe/Zurich')).strftime('%H:%M')
+    st.write(f"Current Time: {current_time}")
+
+    # Button to add a new time-severity entry
+    with st.form(key='severity_form'):
+        severity = st.slider("Severity (1-10)", min_value=1, max_value=10, key=f"severity_slider")
+        if st.form_submit_button("Add Severity"):
+            new_entry = {
+                'time': current_time,
+                'severity': severity
+            }
+            st.session_state.time_severity_entries.append(new_entry)
+            st.success(f"Added entry: Time: {current_time}, Severity: {severity}")
+
+    # Display all time-severity entries
+    for entry in st.session_state.time_severity_entries:
+        st.write(f"Time: {entry['time']}, Severity: {entry['severity']}")
+
+def init_github():
+    """Initialize the GithubContents object."""
+    if 'github' not in st.session_state:
+        st.session_state.github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"])
+        print("github initialized")
+    
+def init_credentials():
+    """Initialize or load the dataframe."""
+    if 'df_users' not in st.session_state:
+        if st.session_state.github.file_exists(DATA_FILE):
+            st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
+        else:
+            st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
+
+def login_page():
+    """Login an existing user."""
+    st.title("Login")
+    with st.form(key='login_form'):
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        if st.form_submit_button("Login"):
+            authenticate(username, password)
+
+def register_page():
+    """Register a new user."""
+    st.title("Register")
+    with st.form(key='register_form'):
+        new_username = st.text_input("New Username")
+        new_name = st.text_input("Name")
+        new_password = st.text_input("New Password", type="password")
+        if st.form_submit_button("Register"):
+            hashed_password = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())  # Hash the password
+            hashed_password_hex = binascii.hexlify(hashed_password).decode()  # Convert hash to hexadecimal string
             
-    else:
-        st.sidebar.write(f"Logged in as {st.session_state['username']}")
-        anxiety_protocol()
+            # Check if the username already exists
+            if new_username in st.session_state.df_users['username'].values:
+                st.error("Username already exists. Please choose a different one.")
+                return
+            else:
+                new_user = pd.DataFrame([[new_username, new_name, hashed_password_hex]], columns=DATA_COLUMNS)
+                st.session_state.df_users = pd.concat([st.session_state.df_users, new_user], ignore_index=True)
+                
+                # Writes the updated dataframe to GitHub data repository
+                st.session_state.github.write_df(DATA_FILE, st.session_state.df_users, "added new user")
+                st.success("Registration successful! You can now log in.")
 
-        logout_button = st.sidebar.button("Logout")
-        if logout_button:
-            st.session_state['authentication'] = False
-            st.session_state.pop('username', None)
-            st.switch_page("main.py")
+def authenticate(username, password):
+    """
+    Authenticate the user.
+
+    Parameters:
+    username (str): The username to authenticate.
+    password (str): The password to authenticate.
+    """
+    login_df = st.session_state.df_users
+    login_df['username'] = login_df['username'].astype(str)
+
+    if username in login_df['username'].values:
+        stored_hashed_password = login_df.loc[login_df['username'] == username, 'password'].values[0]
+        stored_hashed_password_bytes = binascii.unhexlify(stored_hashed_password)  # Convert hex to bytes
+        
+        # Check the input password
+        if bcrypt.checkpw(password.encode('utf8'), stored_hashed_password_bytes): 
+            st.session_state['authentication'] = True
+            st.session_state['username'] = username
+            st.success('Login successful')
             st.experimental_rerun()
+        else:
+            st.error('Incorrect password')
+    else:
+        st.error('Username not found')
+
+def switch_page(page_name):
+    st.success(f"Redirecting to {page_name.replace('_', ' ')} page...")
+    time.sleep(3)
+    st.experimental_set_query_params(page=page_name)
+    st.experimental_rerun()
 
 if __name__ == "__main__":
     main()
