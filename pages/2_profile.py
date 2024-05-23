b76ecc7f9ba5183e6631e8c189148dc27f5e50a1
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 23:17:30 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -43,8 +43,8 @@ def anxiety_assessment():
         if st.button("Yes "):
             switch_page("pages/5_anxiety_protocol.py")
         elif st.button("No "):
+            st.image(gif_url, width=600)
             gif_url = "https://37.media.tumblr.com/28fad0005f6861c08f2c07697ff74aa4/tumblr_n4y0patw7Q1rn953bo1_500.gif"
-            st.image(gif_url, width=400)
 
 def init_github():
     """Initialize the GithubContents object."""
