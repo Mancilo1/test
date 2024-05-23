24accb235a18948b846bd529ef6ce76a03855a55
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-23 20:49:22 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -48,10 +48,7 @@ def main_page():
         if st.button("Login/Register"):
             st.switch_page("pages/1_login.py")
 
-    def switch_page(page_name):
-        st.success("Redirecting to {} page...".format(page_name))
-
- def main():
+def main():
     init_github()
     init_credentials()
 
@@ -136,18 +133,4 @@ def init_github():
     """Initialize the GithubContents object."""
     if 'github' not in st.session_state:
         st.session_state.github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"])
-        print("github initialized")
-    
-def init_credentials():
-    """Initialize or load the dataframe."""
-    if 'df_users' not in st.session_state:
-        if st.session_state.github.file_exists(DATA_FILE):
-            st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
-        else:
-            st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
-
-if __name__ == "__main__":
-    main_page()
+            st.secrets
