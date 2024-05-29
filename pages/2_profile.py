f3b7014486342bfdd92c9645d0ded738ad5bdbef
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-05-29 12:03:04 +0200

    Update 2_profile.py

--- pages/2_profile.py
+++ pages/2_profile.py
@@ -56,7 +56,7 @@ def anxiety_assessment():
 
     if st.session_state.step == 3:
         show_gif()
-        if st.button("Reasses your feelings")
+        if st.button("Reasses your feelings"):
         st.experimental_rerun()
         
 
