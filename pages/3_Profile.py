1626cbb10c072383b9d46e140cb86b39d4f75fba
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-06-02 16:18:02 +0200

    Update 3_Profile.py

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -99,11 +99,12 @@ def main():
             register_page()
     else:
         st.sidebar.write(f"Logged in as {st.session_state['username']}")
-
+        # Retrieve the emergency contact information from the DataFrame
         user_data = st.session_state.df_users.loc[st.session_state.df_users['username'] == st.session_state['username']]
         if not user_data.empty:
             st.session_state['emergency_contact_name'] = user_data['emergency_contact_name'].iloc[0] if 'emergency_contact_name' in user_data.columns else ''
-            st.session_state['emergency_contact_number'] = user_data['emergency_contact_number'].iloc[0] if 'emergency_contact_number' in user_data.columns else ''
+            st.session_state['emergency_contact_number'] = str(user_data['emergency_contact_number'].iloc[0]) if 'emergency_contact_number' in user_data.columns else ''
+
         main_page()
         st.write("---")
         anxiety_assessment()
