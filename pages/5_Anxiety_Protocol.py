ed4e70843c6cf6081499db9ddd0347fbd17180bd
Mancilo1 <162449999+Mancilo1@users.noreply.github.com>
2024-06-03 11:20:24 +0200

    Update 5_Anxiety_Protocol.py

--- pages/5_Anxiety_Protocol.py
+++ pages/5_Anxiety_Protocol.py
@@ -70,7 +70,6 @@ def register_page():
                 # Writes the updated dataframe to GitHub data repository
                 st.session_state.github.write_df(DATA_FILE, st.session_state.df_users, "added new user")
                 st.success("Registration successful! You can now log in.")
-                login_page()
 
 def authenticate(username, password):
     """
